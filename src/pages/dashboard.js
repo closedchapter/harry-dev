456b77680d4d649c161cb77e1925d2003c75a130
Your Name <harrison.november@icloud.com>
2021-07-17 23:15:34 +0100

    commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -17,13 +17,7 @@ const CTA = () => {
   )
 }
 
-
-const Tech = () => {
-  return <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-}
-
-
-const Intro = () => {
+function Intro() {
   return (
     <div className='flex text-black'>
       <div className='flex flex-col'>
@@ -31,7 +25,7 @@ const Intro = () => {
         <div className='text-base font-medium opacity-80 leading-snug'>{"I'm pretty good at coding. In my spare time, I like to create useful apps that help people. My goal is to impact the world one day."}</div>
       </div>
     </div>
-  )
+  );
 }
 
 
